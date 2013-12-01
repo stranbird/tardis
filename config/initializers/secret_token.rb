12a2a9d18d28629b2fa1be4efbff7880a6c03463
stranbird <stranbird@gmail.com>
2013-12-01 16:11:45 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tardis::Application.config.secret_token = '28c390f0945f8067278f28653fe96952668dadb33ab8036a78203f394c793e72d6ab49942572d1cf77f6c4a5a3bcfb00f724eb40cf1ce60172f6875e3d2eb901'
