bc36c9cc1ed50a6e1ee726970faaf49cd1acea4e
stranbird <stranbird@gmail.com>
2013-12-09 16:36:21 +0800

    加入获取当前地点，下载了大量POI照片

---
+++ lib/tasks/importDoubanPOI.rb
@@ -0,0 +1,15 @@
+require 'json'
+
+raw_j = File.read("../../public/poi.json")
+j = JSON.parse(raw_j)
+
+j.each_pair do |k, v|
+  p = Place.new
+  p.longitude = v["longitude"]
+  p.latitude = v["latitude"]
+  p.name = v["name"]
+  p.pic = "/img/" + v["pic"].split("/").last
+  p.user = User.find 1
+  p.save
+end
+
